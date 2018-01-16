d022a1a45bfd3b3c1855f164c1bd5efd543ab5c7
Go Takagi <takagi@shimastripe.com>
2018-01-17 02:29:16 +0900

    Notify github dashboard to slack

--- index.js
+++ index.js
@@ -1,5 +1,149 @@
-module.exports = robot => {
-  robot.respond(/hoge$/i, (res) => {
-    res.send("hoge");
+const GITHUB_DASHBOARD_API = process.env.GITHUB_DASHBOARD_API
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN
+const GH_AC_CHANNEL = process.env.GH_AC_CHANNEL
+
+let cache = [];
+
+const _ = require('lodash');
+const request = require('request');
+const randomColor = require('randomcolor');
+const CronJob = require('cron').CronJob;
+
+const getEvent = async () => {
+  return new Promise((resolve, reject) => {
+    let options = {
+      url: `${GITHUB_DASHBOARD_API}?access_token=${GITHUB_TOKEN}`,
+      method: 'GET',
+      headers: {
+        'Accept': 'application/vnd.github.v3+json',
+        'Content-Type': 'application/json',
+        'User-Agent': 'Hubot',
+      },
+      json: true
+    }
+
+    request(options, (err, res, body) => {
+      if (err) {
+        reject(err);
+        return
+      };
+      resolve(body)
+    });
   });
+};
+
+const formatAtt = (event) => {
+  let att = {
+    color: randomColor(),
+    mrkdwn_in: ["text", "fields"]
+  };
+
+  let repoURL = event.repo.url.replace("api.github.com/repos", "github.com");
+  let repoTag = `*<${repoURL}|${event.repo.name}>*`;
+  let [branchURL, branchTag] = ["", ""];
+
+  if (_.isString(event.payload.ref)) {
+    branchURL = `${event.repo.url.replace("api.github.com/repos", "github.com")}/tree/${event.payload.ref.replace("refs/heads/", "")}`;
+    branchTag = `\`<${branchURL}|${event.payload.ref.replace("refs/heads/", "")}>\``;
+  }
+
+  let text = ''
+  switch (event.type) {
+    case "CreateEvent":
+      if (event.payload.ref === null) {
+        text = `created a ${event.payload.ref_type} at ${repoTag}`;
+      } else {
+        text = `created a ${event.payload.ref_type} ${branchTag} at ${repoTag}`;
+      }
+      att.fallback, att.text = text, text;
+      break;
+    case "PushEvent":
+      text = `pushed to ${branchTag} in ${repoTag}`;
+      att.fallback, att.text = text, text;
+      att.fields = _.map(_.reverse(event.payload.commits), (c) => {
+        let sha = c.sha.slice(0, 7);
+        let shaURL = c.url.replace("api.github.com/repos", "github.com").replace("commits", "commit");
+        let shaTag = `\`<${shaURL}|${sha}>\``;
+        return { value: `${shaTag}  ${c.message}` };
+      });
+      break;
+    case "IssuesEvent":
+      text = `${event.payload.action} an issue in ${repoTag}`;
+      att.fallback, att.text = text, text;
+      att.fields = [
+        { value: `*<${event.payload.issue.html_url}|#${event.payload.issue.number} ${event.payload.issue.title}>*` }
+      ]
+      break;
+    case "MemberEvent":
+      text = `${event.payload.action} *<${event.payload.member.html_url}|${event.payload.member.login}>* to ${repoTag}`;
+      att.fallback, att.text = text, text;
+      break;
+    case "CommitCommentEvent":
+      let sha = event.payload.comment.commit_id.slice(0, 7);
+      let commitTag = `*<${event.payload.comment.html_url}|${event.repo.name}@${sha}>*`;
+      text = `commentd on commit ${commitTag}`;
+      att.fallback, att.text = text, text;
+      att.fields = [
+        { value: `> ${event.payload.comment.body}` }
+      ]
+      break;
+    case "PullRequestEvent":
+      text = `${event.payload.action} an pull request in ${repoTag}`;
+      att.fallback, att.text = text, text;
+      att.fields = [
+        { value: `*<${event.payload.pull_request.html_url}|#${event.payload.pull_request.number} ${event.payload.pull_request.title}>*` }
+      ]
+      break;
+    case "IssueCommentEvent":
+      let issueCommentTag = `*<${event.payload.comment.html_url}|${event.repo.name}#${event.payload.issue.number}>*`;
+      text = `commentd on pull request ${issueCommentTag}`;
+      att.fallback, att.text = text, text;
+      att.fields = [
+        { value: `> ${event.payload.comment.body}` }
+      ]
+      break;
+    case "DeleteEvent":
+      if (event.payload.ref === null) {
+        text = `deleted ${event.payload.ref_type} at ${repoTag}`;
+      } else {
+        text = `deleted ${event.payload.ref_type} ${event.payload.ref.replace("refs/heads/", "")} at ${repoTag}`;
+      }
+      att.fallback, att.text = text, text;
+      break;
+    // case "WatchEvent":
+    //   break;
+    default:
+      text = "拾いきれてないイベントだよ!!報告してください"
+      att.fallback, att.text = text, text;
+      break;
+  }
+
+  return att;
+};
+
+module.exports = robot => {
+  if (!(GITHUB_DASHBOARD_API && GITHUB_TOKEN && GH_AC_CHANNEL)) {
+    return;
+  }
+
+  new CronJob('*/5 * * * * *', async () => {
+    robot.logger.debug("Get github dashboard");
+    let eventList = await getEvent();
+
+    if (cache.length === 0) {
+      cache = eventList;
+      return;
+    }
+
+    _.forEach(_.reverse(_.differenceWith(eventList, cache, _.isEqual)), (event) => {
+      robot.messageRoom(GH_AC_CHANNEL, {
+        "username": event.actor.display_login,
+        "icon_url": event.actor.avatar_url,
+        "attachments": [formatAtt(event)],
+        "as_user": false
+      });
+    });
+
+    cache = eventList;
+  }, null, true, 'Asia/Tokyo');
 }
